66c77a8e420121ac3704cdadbeb2e27354ee0cda
mimilai@fas.harvard.udu <mimilai@fas.harvard.edu>
2014-12-18 15:21:08 -0500

    Clean navigation

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -21,6 +21,55 @@
 
 	<a href='/'><img class='logo' src='/images/secret_family_recipes.png' alt='Foorecipes logo'></a>
 </center>
+	
+	
+	
+
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+			
+			<center>
+
+				<a href='/'><img class='logo' src='/images/secret_family_recipes.png' alt='Recipes logo'></a>
+			</center>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div><!--navbar-header-->
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+				
+				@if(Auth::check())
+				<!--	<li id="navhome"><a href="/"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+					<li id="navlorem"><a href="/lorem-ipsum"><span class="glyphicon glyphicon-pencil"></span> Lorem Ipsum</a></li>
+					<li id="navfaker"><a href="/faker"><span class="glyphicon glyphicon-user"></span> Fake Users</a></li>
+				
+				-->
+				<li id="navhome"><a href="/"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+				<li><a href='/recipe/create'><span class="glyphicon glyphicon-pencil"></span> Add Recipe</a></li>
+					
+				<li><a href='/recipe'><span class="glyphicon glyphicon-th-large"></span> View All Recipes</a></li>
+				<li><a href='/recipe/search'><span class="glyphicon glyphicon-search"></span> Search Recipes</a></li>
+				<li><a href='/tag'><span class="glyphicon glyphicon-tag"></span> Recipe Tags</a></li>
+				
+				
+				<li><a href='/logout'><span class="glyphicon glyphicon-cog"></span> Logout {{ Auth::user()->email; }}</a></li>
+				
+				@else
+					<li><a href='/signup'><span class="glyphicon glyphicon-user"></span> Sign up</a> ! <a href='/login'>Log in</a></li>
+				@endif
+		
+			</ul>
+			</div><!--nav-collapse -->
+		</div><!--container-->
+	</div><!--navbar-->	
+	
+	<!--
+	
 	<nav>
 		<ul>
 		@if(Auth::check())
@@ -34,7 +83,7 @@
 			<li><a href='/signup'>Sign up</a> ! <a href='/login'>Log in</a></li>
 		@endif
 		</ul>
-	</nav>
+	</nav>-->
 
 	@yield('content')
 
@@ -43,6 +92,8 @@
 
 </body>
 </html>
+
+
 
 
 
